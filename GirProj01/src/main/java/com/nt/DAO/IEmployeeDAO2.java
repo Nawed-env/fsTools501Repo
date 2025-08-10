0ffc077e7215c731bab2e3034b77401f1c69b6bc
Nawed-env <nawedalam1972@gmail.com>
2025-08-10 14:10:46 +0530

    Delete GirProj01 directory

--- GirProj01/src/main/java/com/nt/DAO/IEmployeeDAO2.java
+++
@@ -1,9 +0,0 @@
-package com.nt.DAO;
-
-import com.nt.bean.Employee;
-
-public interface IEmployeeDAO2 {
-		
-	//inserting data using MySQL
-	public  int insertEmployeeMySQL(Employee emp3) throws Exception;
-}
--- GirProj01/src/main/java/com/nt/service/serviceInteface2.java
+++
@@ -1,9 +0,0 @@
-package com.nt.service;
-
-import com.nt.bean.Employee;
-
-public interface serviceInteface2 {
-	
-	public String registerDataToMySQL(Employee emp3) throws Exception;
-
-}
